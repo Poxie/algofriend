bd279ca9cfe664407a08e85d50c2e371801492e9
Poxie <poxen12@hotmail.com>
2022-07-23 22:36:05 +0200

    Created the button component

---
+++ frontend/components/button/index.tsx
@@ -0,0 +1,25 @@
+import styles from './Button.module.scss';
+
+export const Button: React.FC<{
+    children: any;
+    onClick?: () => void;
+    disabled?: boolean;
+    className?: string;
+    type?: 'default' | 'secondary' | 'hollow' | 'transparent';
+}> = ({ children, onClick, disabled, className, type='default' }) => {
+    className = [
+        className ? className : '',
+        styles['container'],
+        styles[type],
+        disabled ? styles['disabled'] : ''
+    ].join(' ');
+    return(
+        <button
+            onClick={!disabled ? onClick : undefined}
+            className={className}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    )
+}
