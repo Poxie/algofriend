311990bd99763faeddc2b295ceb6d7524322bcdc
Poxie <poxen12@hotmail.com>
2022-07-24 19:35:34 +0200

    Added hover state to algorithm items

--- frontend/components/algorithms/AlgorithmRow.tsx
+++ frontend/components/algorithms/AlgorithmRow.tsx
@@ -3,11 +3,11 @@ import { AlgorithmRow as AlgorithmRowType } from "../../assets/algorithms/types"
 import { AlgorithmRowItems } from "./AlgorithmRowItems"
 import { CSSProperties } from 'react';
 
-export const AlgorithmRow: React.FC<AlgorithmRowType> = ({ title, color, colorSecondary, items }) => {
+export const AlgorithmRow: React.FC<AlgorithmRowType> = ({ id, title, items }) => {
     return(
         <div 
             className={styles['row']}
-            style={{ '--primary': color, '--secondary': colorSecondary } as CSSProperties}
+            style={{ '--primary': `var(--${id}-color-primary)`, '--secondary': `var(--${id}-color-secondary)`, '--hover': `var(--${id}-color-hover)` } as CSSProperties}
         >
             <h2 className={styles['header']}>
                 {title}
