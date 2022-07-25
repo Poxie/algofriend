47ea0b39bc6c3c74d911da79e16ceeaa957084e4
Poxie <poxen12@hotmail.com>
2022-07-25 16:01:36 +0200

    Added compatibility for page layouts

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,26 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Navbar } from '../components/navbar/Navbar'
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return(
     <>
     <Navbar />
-    <Component {...pageProps} />
+    {getLayout(
+      <Component {...pageProps} />
+    )}
     </>
   )
 }
