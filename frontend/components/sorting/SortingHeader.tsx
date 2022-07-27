d4733f926f913cc8ac819d6362e731f6274a91d8
Poxie <poxen12@hotmail.com>
2022-07-27 03:08:17 +0200

    Added color explanation

--- frontend/components/sorting/SortingHeader.tsx
+++ frontend/components/sorting/SortingHeader.tsx
@@ -1,9 +1,26 @@
 import styles from '../../styles/Sorting.module.scss';
 
+const items = [
+    { title: 'Finished', color: 'lime' },
+    { title: 'Correct order', color: 'lightblue' },
+    { title: 'Incorrect order', color: 'red' },
+    { title: 'Neutral', color: 'var(--color-secondary)' }
+]
 export const SortingHeader = () => {
     return(
         <div className={styles['header']}>
-            {/* Add visual stats here */}
+            {items.map(item => (
+                <div className={styles['header-item']}>
+                    <div 
+                        style={{ backgroundColor: item.color }}
+                        className={styles['header-color']}
+                    />
+                    
+                    <span>
+                        {item.title}
+                    </span>
+                </div>
+            ))}
         </div>
     )
 }
